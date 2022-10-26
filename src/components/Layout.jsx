62345bc1dace7fbe9f18a50cda18e6b913467df5
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-25 20:04:51 -0500

    definicion de rutas y elementos en comun

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div className='md-flex md:min-h-screen'>
+      <div className='md:w-1/4 bg-blue-600 px-5 py-10'>
+      </div>
+      <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+
+      </main>
+    </div>
+  )
+}
+
+export default Layout
