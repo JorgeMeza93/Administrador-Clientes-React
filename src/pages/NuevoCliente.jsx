53e527733d384d4db10356f3939b29e583c3435b
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-27 00:07:30 -0500

    Verificacion del form data

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Form} from 'react-router-dom';
 import Formulario from '../components/Formulario';
 
+export async function action({request}){
+  const formData = await request.formData()
+  console.log(formData)
+}
+
 const NuevoCliente = () => {
+  
   const navigate = useNavigate();
   return (
     <div>
@@ -17,15 +23,17 @@ const NuevoCliente = () => {
         </button>
       </div>
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
-        <form>
+        <Form
+          method='post'
+        >
           <Formulario/>
           <input type="submit"
             className='mt-5 w-full bg-blue-700 p-3 uppercase font-bold text-white text-lg'
             value="Registar Cliente"
           />
-        </form>
+        </Form>
       </div>
-      
+
     </div>
   )
 }
