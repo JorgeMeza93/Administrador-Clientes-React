880b4c7bcbabbf32b68465ccb311d13709c8d5fc
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-28 23:17:27 -0500

    Verificar que el cliente exista y mostrar error

--- src/data/Clientes.jsx
+++ src/data/Clientes.jsx
@@ -19,4 +19,10 @@ async function agregarCliente(datos){
     }
 }
 
-export { obtenerClientes, agregarCliente }
+async function obtenerCliente(id){
+    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
+    const resultado = await respuesta.json();
+    return resultado;
+}
+
+export { obtenerClientes, agregarCliente, obtenerCliente }
--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { obtenerCliente } from '../data/Clientes';
 
 export async function loader({params}){
-    console.log(params);
+    const cliente = await obtenerCliente(params.clienteId);
+    if(Object.values(cliente).length === 0){
+      throw new Response("", {
+        status: 404,
+        statusText: "No hay resultados"
+      })
+    }
+    return cliente;
 }
 
 const EditarCliente = () => {
