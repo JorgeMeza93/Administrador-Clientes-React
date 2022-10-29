bb811c7b9944ab7da09e421dfe660d9a1aac4a2d
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-29 00:01:28 -0500

    Recuperar info en el formulario al editar cliente

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { obtenerCliente } from '../data/Clientes';
+import { Form, useNavigate, useLoaderData } from 'react-router-dom';
+import Formulario from '../components/Formulario';
 
 export async function loader({params}){
     const cliente = await obtenerCliente(params.clienteId);
@@ -13,8 +15,40 @@ export async function loader({params}){
 }
 
 const EditarCliente = () => {
+  const navigate = useNavigate();
+  const cliente = useLoaderData();
+  console.log(cliente);
   return (
-    <div>EditarCliente</div>
+    <div>
+       <h1 className='font-black text-4xl text-blue-900'>Editar Cliente</h1>
+       <p className='mt-3'>A continuación podrás modificar los datos de un cliente</p>
+      <div className='flex justify-end'>
+        <button
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+          onClick={ () => navigate("/")}
+        >
+          Volver
+        </button>
+      </div>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        {/*errores?.length && errores.map( (error, i) => {
+          return <Error key={i}>{error}</Error>
+        })*/}
+        <Form
+          method='post'
+          noValidate
+        >
+          <Formulario
+            cliente={cliente}
+          />
+          <input type="submit"
+            className='mt-5 w-full bg-blue-700 p-3 uppercase font-bold text-white text-lg'
+            value="Registar Cliente"
+          />
+        </Form>
+      </div>
+
+    </div>
   )
 }
 
